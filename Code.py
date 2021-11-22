47933a5ba679315ef47ab30a2fa6a9882736e873
Sahil <75215825+SahilSharma-20@users.noreply.github.com>
2021-11-22 23:12:08 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -67,7 +67,7 @@ if response.status_code == 200:
 		"\n humidity (in percentage) = " +
 					str(current_humidity) +
 		"\n description = " +
-					str(weather_description))
+					str(weather_description))kkkkkkkkk
 
 else:
 	print(" City Not Found ")
