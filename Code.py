f9bb5b76dfd507119f37463cc06ea46303432ad5
Sahil <75215825+SahilSharma-20@users.noreply.github.com>
2021-12-24 21:04:26 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,73 +1,119 @@
-# Python program to find current
-# weather details of any city
-# using openweathermap api
-#pip install simplejson
-# import required modules
+%reset -f
+
 import requests, json
-#Git Test
-# Enter your API key here
-api_key = "a187a56de525ef3655614182134f92d5"
 
-# base_url variable to store url
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+import gtts
 
-# Give city name
-city_name = input("Enter city name : ")
+import demoji
 
-# complete_url variable to store
-# complete url address
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+import os
 
-# get method of requests module
-# return response object
-response = requests.get(complete_url)
+from playsound import playsound
 
-# json method of response object
-# convert json format data into
-# python format data
-x = response.json()
+cities = input('Enter Number_of_cities:')
 
-# Now x contains list of nested dictionaries
-# Check the value of "cod" key is equal to
-# "404", means city is found otherwise,
-# city is not found
-#if x["cod"] != "404":
-if response.status_code == 200:
+for i in range(int(cities)):    
 
-	# store the value of "main"
-	# key in variable y
-	y = x["main"]
+    api_key = "db9f053143446cc58b988adc4959b3c2"
 
-	# store the value corresponding
-	# to the "temp" key of y
-	current_temperature = y["temp"]
+    # base_url variable to store url
 
-	# store the value corresponding
-	# to the "pressure" key of y
-	current_pressure = y["pressure"]
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-	# store the value corresponding
-	# to the "humidity" key of y
-	current_humidity = y["humidity"]
+ 
 
-	# store the value of "weather"
-	# key in variable z
-	z = x["weather"]
+    city_name = input("Enter city name : ")
 
-	# store the value corresponding
-	# to the "description" key at
-	# the 0th index of z
-	weather_description = z[0]["description"]
+ 
 
-	# print following values
-	print(" Temperature (in centigrade unit) = " +
-					str(current_temperature - 273.15) +
-		"\n atmospheric pressure (in hPa unit) = " +
-					str(current_pressure) +
-		"\n humidity (in percentage) = " +
-					str(current_humidity) +
-		"\n description = " +
-					str(weather_description))
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
-else:
-	print(" City Not Found ")
+ 
+
+    response = requests.get(complete_url)
+
+ 
+
+    x = response.json()
+
+    if  x["cod"] != "404":
+
+        y = x["main"]
+
+        current_temperature = y["temp"]-273.15
+
+        current_pressure = y["pressure"]
+
+        current_humidity = y["humidity"]
+
+        z = x["weather"]
+
+        weather_description = z[0]["description"]
+
+ 
+
+        print(" Temperature (in celsius unit) =" +
+
+                    str(round(current_temperature,2))
+
+                        + " °C "
+
+          "\n atmospheric pressure (in hPa unit) = " +
+
+                    str(current_pressure) + " hPa "
+
+          "\n humidity (in percentage) = " +
+
+                    str(current_humidity) + " % "
+
+          "\n description = " +
+
+                    str(weather_description))
+
+    else:
+
+        print(" City Not Found ")
+
+    Sunny = "😰"
+
+    Cool = "🤧"
+
+    language="en"
+
+    if current_temperature >27 and current_temperature < 30:
+
+        print("Sunny="+str(demoji.findall(Sunny)))
+
+        txt = gtts.gTTS("Sunny",lang=language,slow=False)
+
+        txt.save("media.mp3")    
+
+        playsound("media.mp3")
+
+    else:
+
+        print("Cool="+str(demoji.findall(Cool)))
+
+        txt = gtts.gTTS("Cool")
+
+        txt.save("media.mp3")    
+
+        playsound("media.mp3")
+
+    os.remove('media.mp3')
+
+   
+
+    txt = gtts.gTTS('Display wheather report for:-'+ city_name)
+
+    txt.save("media.mp3")    
+
+    playsound("media.mp3")
+
+    os.remove('media.mp3')
+
+    url = 'https://wttr.in/{}'.format(city_name)
+
+    res = requests.get(url)
+
+    print(res.text)
